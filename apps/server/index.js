d1288d8693cf0108087aedb693f2d515a792b245
Fafi03 <138723875+Fafi03@users.noreply.github.com>
2026-04-30 00:00:00 +0300

    server setup

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const PORT = 5000;
 
-app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Smart-Draft AI Server is Running');
+    res.send('Smart-Draft AI Server is running...');
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
